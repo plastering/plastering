275fe9fc68921eae8da9cd3004f5ae291ebcbff1
Jason Koh <bk7749@gmail.com>
2018-01-03 19:51:17 -0800

    add ground truth converter from scrabble

---
+++ rawdata/metadata/scrabble2oracle.py
@@ -0,0 +1,66 @@
+import json
+import pdb
+from collections import defaultdict
+from functools import reduce
+from copy import deepcopy
+
+import pandas as pd
+import numpy as np
+
+from jasonhelper import argparser
+
+
+# Helper functions
+
+adder = lambda x, y: x + y
+
+def normalize_phrase(phrase):
+    if type(phrase) == str:
+        return phrase.lower()
+    else:
+        if np.isnan(phrase):
+            return ''
+        else:
+            assert False
+
+argparser.add_argument('-b', 
+                       type=str, 
+                       help='Target building name', 
+                       dest='building')
+args = argparser.parse_args()
+
+building = args.building
+
+
+# Transform parsed lables used in Scrabble for Oracle.
+
+df = pd.read_csv('{0}_rawmetadata.csv'.format(building))\
+           .set_index('SourceIdentifier')
+with open('{0}_char_label_dict.json'.format(building), 'r') as fp:
+    char_label_dict = json.load(fp)
+
+parse_dict = dict()
+
+used_col_names = ['BACnetName', 'VendorGivenName', 'BACnetDescription']
+for srcid, char_label_pairs in char_label_dict.items():
+    row = df.loc[srcid]
+    sentence = reduce(adder, [normalize_phrase(row[col_name]) + '\n' 
+                              for col_name in used_col_names], '')
+    assert ''.join([pair[0] for pair in char_label_pairs]) == sentence
+    sent_it = iter(char_label_pairs)
+    pairs = deepcopy(char_label_pairs)
+    col_parse_dict = defaultdict(list)
+    for col_name in used_col_names:
+        phrase = normalize_phrase(row[col_name])
+        for _ in phrase:
+            char_label_pair = char_label_pairs.pop(0)
+            col_parse_dict[col_name].append(char_label_pair)
+        [char, _] = char_label_pairs.pop(0)
+        assert char == '\n'
+    parse_dict[srcid] = dict(col_parse_dict)
+    assert len(char_label_pairs) == 0
+
+with open('../../groundtruth/{0}_full_parsing.json'.format(building), 'w') \
+         as fp:
+    json.dump(parse_dict, fp, indent=2)
+
