be025d38cd8eda8d164c1245d042deaf0392be2d
Jason Koh <bk7749@gmail.com>
2018-05-02 20:42:57 -0700

    add helper to load UVA metadata and labels

---
+++ oracle/helper/load_uva.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from ..metadata_interface import *
+
+def load_uva_building(building='uva_cse',
+                      filename='./groundtruth/uva_cse_point_map.csv'):
+    df = pd.read_csv('./groundtruth/uva_cse_point_map.csv')
+    for i, row in df.iterrows():
+        srcid = row['original label']
+        tagset = row['tagset']
+
+        # Store raw metadata
+        raw_obj = RawMetadata.objects(srcid=srcid)\
+            .upsert_one(srcid=srcid, building=building)
+        raw_obj.metadata['VendorGivenName'] = srcid
+        raw_obj.save()
+
+        # Store ground truth
+        labeled_obj = LabeledMetadata.objects(srcid=srcid)\
+            .upsert_one(srcid=srcid, building=building)
+        labeled_obj.point_tagset = tagset
+        labeled_obj.save()
+
