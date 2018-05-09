322f9350a271aa6b677b26a5aa98d22af42f7200
Jason Koh <bk7749@gmail.com>
2018-05-08 23:16:34 -0700

    first working draft of `evaluation`

--- test_zodiac.py
+++ test_zodiac.py
@@ -1,13 +1,14 @@
 from plastering.inferencers.zodiac_interface import ZodiacInterface
 from plastering.metadata_interface import *
 
-target_building = 'ap_m'
+target_building = 'uva_cse'
 
 # Select labeled srcids (Not all the data are labeled yet.)
 labeled_list = LabeledMetadata.objects(building=target_building)
 target_srcids = [labeled['srcid'] for labeled in labeled_list]
 
-zodiac = ZodiacInterface(target_building, target_srcids)
+zodiac = ZodiacInterface(target_building=target_building,
+                         target_srcids=target_srcids)
 zodiac.learn_auto()
 pred = zodiac.predict()
 proba = zodiac.predict_proba()
