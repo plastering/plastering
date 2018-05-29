85a7e1cc182e536cf779c606a6a38a69d54ff75c
Jason Koh <bk7749@gmail.com>
2018-05-29 12:20:08 -0700

    add Marco's ahu-vav infer

--- plastering/inferencers/quiver/__init__.py
+++ plastering/inferencers/quiver/__init__.py
@@ -3,7 +3,7 @@ from ...rdflib_wrapper import *
 
 from uuid import uuid4 as gen_uuid
 
-class DummyQuiver(GroundTruthInterface):
+class DummyQuiver(Inferencer):
 
     def __init__(self,
                  ground_truth_ttl,
@@ -47,9 +47,86 @@ class DummyQuiver(GroundTruthInterface):
             random_obj = create_uri(str(gen_uuid())) # This would be a VAV.
             for point in points:
                 insert_triple(pred_g, (point, BF['isPointOf'], random_obj))
+                insert_triple(pred_g, (random_obj, RDF['type'], BRICK['VAV']))
 
         pred_g.serialize('test.ttl', format='turtle')
         return pred_g
 
-class DummyPritoniEtal(Inferencer):
-    pass
+class DummyPritoni(Inferencer):
+
+    def __init__(self,
+                 ground_truth_ttl,
+                 target_building,
+                 target_srcids,
+                 source_buildings=[],
+                 ui=None,
+                 config={}):
+        self.true_g = init_graph()
+        self.true_g.parse(ground_truth_ttl, format='turtle')
+        super(DummyPritoni, self).__init__(
+            target_building=target_building,
+            target_srcids=target_srcids,
+            ui=ui,
+            config=config,
+            framework_name='quiver')
+
+    def get_ahu_datsp(self): #discharge air temperature setpoint
+        qstr = """
+        select ?datsp ?ahu where {
+        ?datsp a/rdfs:subClassOf* brick:Discharge_Air_Temperature_Setpoint.
+        ?datsp bf:isPointOf ?ahu.
+        ?ahu a brick:AHU.
+        }
+        """
+        res = query_sparql(self.prior_g, qstr)
+        return [
+            {
+                'datsp': row['datsp'],
+                'ahu': row['ahu']
+            } for row in res
+        ]
+
+    def get_all_vavs_with_znt(self):
+        qstr = """
+        select ?vav where {
+        ?vav a/rdfs:subClassOf* brick:VAV .
+        ?znt bf:isPointOf ?vav.
+        ?znt a brick:Zone_Temperature_Sensor.
+        }
+        """
+        res = query_sparql(self.prior_g, qstr)
+        return [row['vav'] for row in res]
+
+    def predict(self):
+        pred_g = init_graph()
+        ahu_datsps = self.get_ahu_datsp()
+        found_vavs = self.get_all_vavs_with_znt()
+
+        for row in ahu_datsps:
+            ahu = row['ahu']
+            datsp = row['datsp']
+            qstr = """
+            select ?vav where {{
+              {0} bf:feeds+ ?vav.
+              ?vav a/rdfs:subClassOf* brick:VAV.
+            }}
+            """.format(ahu.n3())
+            pdb.set_trace()
+            res = query_sparql(self.true_g, qstr)
+            true_vavs = [row['vav'] for row in res]
+            pred_vavs = [vav for vav in true_vavs if vav in found_vavs]
+            for vav in pred_vavs:
+                insert_triple(pred_g, (ahu, BF['feeds'], vav))
+
+        pred_g.serialize('test.ttl', format='turtle')
+        return pred_g
+
+
+
+
+
+
+
+
+
+
