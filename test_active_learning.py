bedafa022a1079d45f7e777f0195ae36270151d4
Dezhi Hong <dzhong1989@gmail.com>
2018-05-30 21:20:16 -0400

    added snippet for results writing

--- test_active_learning.py
+++ test_active_learning.py
@@ -1,3 +1,6 @@
+import json
+import numpy as np
+
 from plastering.inferencers.active_learning_interface import ActiveLearningInterface
 from plastering.metadata_interface import *
 
@@ -16,4 +19,14 @@ al = ActiveLearningInterface(
     )
 
 al.run_auto()
+acc_sum = [np.nanmean(i) for i in al.learner.acc_sum]
+f1_micro_sum = [np.nanmean(i) for i in al.learner.f1_micro_sum]
+f1_macro_sum = [np.nanmean(i) for i in al.learner.f1_macro_sum]
 
+results = zip(acc_sum, f1_micro_sum, f1_macro_sum)
+outputs = [{ 'metrics': { 'accuracy': acc, 'f1_micro': f1_micro, 'f1_macro':f1_macro }, \
+            'learning_srcids': i+1 } \
+            for i,(acc,f1_micro,f1_macro) in enumerate(results) \
+            ]
+with open('result/al_pointonly_{0}_{1}.json'.format('al_hong', target_building), 'w') as fp:
+    json.dump(outputs, fp)
