1b2d9ae9c92a9c7b471515d268e45a9c3fc3ea5d
Jason Koh <bk7749@gmail.com>
2018-06-01 18:16:49 -0700

    merge

--- test_active_learning.py
+++ test_active_learning.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import numpy as np
+import pdb
 
 from plastering.inferencers.active_learning_interface import ActiveLearningInterface
 from plastering.metadata_interface import *
@@ -11,7 +12,7 @@ labeled_list = LabeledMetadata.objects(building=target_building)
 target_srcids = [labeled['srcid'] for labeled in labeled_list]
 
 fold = 10
-rounds = 100
+rounds = 250
 al = ActiveLearningInterface(
     target_building,
     target_srcids,
@@ -19,15 +20,16 @@ al = ActiveLearningInterface(
     rounds=rounds
     )
 
-al.run_auto()
+al.learn_auto()
 acc_sum = [np.nanmean(i) for i in al.learner.acc_sum]
 f1_micro_sum = [np.nanmean(i) for i in al.learner.f1_micro_sum]
 f1_macro_sum = [np.nanmean(i) for i in al.learner.f1_macro_sum]
 
 results = zip(acc_sum, f1_micro_sum, f1_macro_sum)
-outputs = [{ 'metrics': { 'accuracy': acc, 'f1_micro': f1_micro, 'f1_macro':f1_macro }, \
+outputs = [{ 'metrics': { 'accuracy': acc, 'f1': f1_micro, 'macrof1':f1_macro }, \
+    # to Jason: revert the name
             'learning_srcids': i+1 } \
             for i,(acc,f1_micro,f1_macro) in enumerate(results) \
             ]
-with open('result/al_pointonly_{0}_{1}.json'.format('al_hong', target_building), 'w') as fp:
+with open('result/pointonly_notransfer_{0}_{1}_0.json'.format('al_hong', target_building), 'w') as fp:
     json.dump(outputs, fp)
