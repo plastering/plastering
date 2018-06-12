b526d8ec18da770efa42f5b667efdd55fa7429e4
Dezhi Hong <dzhong1989@gmail.com>
2018-06-12 03:23:27 -0400

    added feature storing/loading for BA, debugging the algorithm

--- plastering/inferencers/building_adapter_interface.py
+++ plastering/inferencers/building_adapter_interface.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 import time
 import pdb
+import pickle as pk
 
 from sklearn.feature_extraction.text import CountVectorizer as CV
 from sklearn.preprocessing import LabelEncoder as LE
@@ -33,7 +34,6 @@ def get_data_features(building, start_time, end_time):
 
     X = []
     srcids = []
-    #for point, data in res.items():
     ctr = 0
     ctr1 = 0
     for labeled in LabeledMetadata.objects(building=building):
@@ -43,23 +43,21 @@ def get_data_features(building, start_time, end_time):
             data = res[srcid]
         except:
             print (srcid, 'not found and skipped.')
-            ctr += 1
             continue
 
-        #t0 = time.clock()
         #TODO: better handle the dimension, it's really ugly now
-
         #computing features on long sequence is really slow now, so only loading a small port of the readings
         try:
             df = data['data'][:3000]
-            if len(df) < 600: #discard really short sequences
+            if len(df) < 400: #discard short sequences
+                ctr += 1
                 continue
             else:
                 X.append( df )
         except:
             pdb.set_trace()
+
         srcids.append(srcid)
-        #print (time.clock() - t0)
 
     print (ctr,'out of',ctr1,'points timeseries not loaded')
 
@@ -93,6 +91,7 @@ class BuildingAdapterInterface(Inferencer):
                  target_srcids,
                  source_buildings,
                  config={},
+                 load_from_file=0
                  ):
         super(BuildingAdapterInterface, self).__init__(
             target_building=target_building,
@@ -121,46 +120,60 @@ class BuildingAdapterInterface(Inferencer):
 
         source_building = source_buildings[0]
 
-        #data features
-        source_ids, train_fd = get_data_features(source_building,
-                                                 self.source_time_ranges[0][0],
-                                                 self.source_time_ranges[0][1])
-        target_ids, test_fd = get_data_features(target_building,
-                                                self.target_time_range[0],
-                                                self.target_time_range[1])
+        if not load_from_file:
+            #data features
+            source_ids, train_fd = get_data_features(source_building,
+                                                     self.source_time_ranges[0][0],
+                                                     self.source_time_ranges[0][1])
+            target_ids, test_fd = get_data_features(target_building,
+                                                    self.target_time_range[0],
+                                                    self.target_time_range[1])
 
-        #name features, labels
-        source_res = get_namefeatures_labels(source_building)
-        train_label = [source_res[srcid][1] for srcid in source_ids]
+            #name features, labels
+            source_res = get_namefeatures_labels(source_building)
+            train_label = [source_res[srcid][1] for srcid in source_ids]
 
-        target_res = get_namefeatures_labels(target_building)
-        test_fn = np.asarray( [target_res[tgtid][0] for tgtid in target_ids] )
-        test_label = [target_res[tgtid][1] for tgtid in target_ids]
+            target_res = get_namefeatures_labels(target_building)
+            test_fn = np.asarray( [target_res[tgtid][0] for tgtid in target_ids] )
+            test_label = [target_res[tgtid][1] for tgtid in target_ids]
 
-        #find the label intersection
-        intersect = list( set(test_label) & set(train_label) )
-        print ('intersected tagsets:', intersect)
+            #find the label intersection
+            intersect = list( set(test_label) & set(train_label) )
+            print ('intersected tagsets:', intersect)
 
-        #preserve the intersection, get ids for indexing data feature matrices
-        if intersect:
-            train_filtered = [[i,j] for i,j in enumerate(train_label) if j in intersect]
-            train_id, train_label = [list(x) for x in zip(*train_filtered)]
-            test_filtered = [[i,j] for i,j in enumerate(test_label) if j in intersect]
-            test_id, test_label = [list(x) for x in zip(*test_filtered)]
+            #preserve the intersection, get ids for indexing data feature matrices
+            if intersect:
+                train_filtered = [[i,j] for i,j in enumerate(train_label) if j in intersect]
+                train_id, train_label = [list(x) for x in zip(*train_filtered)]
+                test_filtered = [[i,j] for i,j in enumerate(test_label) if j in intersect]
+                test_id, test_label = [list(x) for x in zip(*test_filtered)]
+            else:
+                raise ValueError('no common labels!')
+
+            train_fd = train_fd[train_id, :]
+            test_fd = test_fd[test_id, :]
+            #test_fn = [test_fn[tid] for tid in test_id]
+            test_fn = test_fn[test_id, :]
+            print ('%d training examples left'%len(train_fd))
+            print ('%d testing examples left'%len(test_fd))
+
+            le = LE()
+            le.fit(intersect)
+            train_label = le.transform(train_label)
+            test_label = le.transform(test_label)
+
+            res = [train_fd, test_fd, train_label, test_label, test_fn]
+            with open('./%s-%s.pkl'%(source_building,target_building), 'wb') as wf:
+                pk.dump(res, wf)
         else:
-            raise ValueError('no common labels!')
+            with open('./%s-%s.pkl'%(source_building,target_building), 'rb') as rf:
+                res = pk.load(rf)
+            train_fd, test_fd, train_label, test_label, test_fn = \
+            res[0], res[1], res[2], res[3], res[4]
 
-        train_fd = train_fd[train_id, :]
-        test_fd = test_fd[test_id, :]
-        #test_fn = [test_fn[tid] for tid in test_id]
-        test_fn = test_fn[test_id, :]
-        print ('%d training examples left'%len(train_fd))
-        print ('%d testing examples left'%len(test_fd))
-
-        le = LE()
-        le.fit(intersect)
-        train_label = le.transform(train_label)
-        test_label = le.transform(test_label)
+        print ( '# of classes:', len(set(train_label)) )
+        print ( 'data features for %s with dim:'%source_building, train_fd.shape)
+        print ( 'data features for %s with dim:'%target_building, test_fd.shape)
 
 
         self.learner = transfer_learning(
