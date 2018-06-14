43a80c422a62b0987ce36e546e8440ca4a76868a
Dezhi Hong <dzhong1989@gmail.com>
2018-06-14 19:44:58 -0400

    optimized

--- test_feature_selector.py
+++ test_feature_selector.py
@@ -1,6 +1,9 @@
 from plastering.feature_selector import *
+import sys
 
-target_building = 'ap_m'
+target_building = sys.argv[1]
+load_from_file = int(sys.argv[2])
+method = "lsvc"
 
-fs = feature_selector(target_building, 1, 0)
+fs = feature_selector(target_building, method, load_from_file)
 fs.run_auto()
