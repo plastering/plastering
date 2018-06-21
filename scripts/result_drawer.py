486037a0f03d71e0b83a4ecdac943562bbc44a4b
Jason Koh <bk7749@gmail.com>
2018-06-21 03:03:33 -0700

    update

--- scripts/result_drawer.py
+++ scripts/result_drawer.py
@@ -34,8 +34,9 @@ ACC_COLOR = 'darkgreen'
 
 inferencer_display_names = {
     'scrabble': 'Scrabble',
-    'al_hong': 'Hong et al.',
+    'al_hong': 'Hong',
     'zodiac': 'Zodiac',
+    'arka': 'Bhattacharya'
 }
 
 def average_data(xs, ys, target_x):
@@ -47,10 +48,14 @@ def average_data(xs, ys, target_x):
 
 def plot_pointonly_notransfer():
     EXP_NUM = 4
-    inferencer_names = ['zodiac', 'al_hong', 'scrabble']
+    inferencer_names = ['zodiac', 'al_hong', 'scrabble', 'arka']
     buildings = ['ebu3b', 'uva_cse', 'sdh', 'ghc']
     #buildings = ['sdh', 'ebu3b']
     outputfile = FIG_DIR + '/pointonly_notransfer.pdf'
+    linestyle_dict = {
+        'arka': ':',
+        'scrabble': '--'
+    }
 
     fig, axes = plt.subplots(1, len(buildings))
     xticks = [0, 10] + list(range(50, 251, 50))
@@ -69,12 +74,20 @@ def plot_pointonly_notransfer():
         for inferencer_name in inferencer_names:
             if building == 'uva_cse' and inferencer_name == 'scrabble':
                 continue
+            if inferencer_name == 'arka':
+                if building != 'sdh':
+                    continue
+            if inferencer_name == 'arka':
+                exp_num = 1
+            else:
+                exp_num = EXP_NUM
+
             xs = []
             ys = []
             xss = []
             f1s = []
             mf1s = []
-            for i in range(0, EXP_NUM):
+            for i in range(0, exp_num):
                 with open('result/pointonly_notransfer_{0}_{1}_{2}.json'
                           .format(inferencer_name, building, i)) as  fp:
                     data = json.load(fp)
@@ -92,7 +105,7 @@ def plot_pointonly_notransfer():
             mf1 = average_data(xss, mf1s, interp_x)
             x = interp_x
             ys = [f1, mf1]
-            if ax_num == 0:
+            if ax_num == 2:
                 legends = [
                     'micro-F1, {0}'.format(
                         inferencer_display_names[inferencer_name]),
@@ -111,13 +124,13 @@ def plot_pointonly_notransfer():
                 yticks, yticks_labels, title, ax, fig, ylim, xlim, legends,
                 linestyles=[linestyles.pop()]*len(ys), cs=colors,
                 xtickRotate=xtickRotate)
-            if ax_num == 0 and inferencer_name == 'scrabble':
+            if ax_num == 2 and inferencer_name in ['scrabble', 'arka']:
                 _, plots = plotter.plot_multiple_2dline(
                     [-10], [[-10]], xlabel, ylabel, xticks, xticks_labels,
                     yticks, yticks_labels, title, ax, fig, ylim, xlim,
                     ['Accuracy, {0}'.format(
                         inferencer_display_names[inferencer_name])],
-                    linestyles=['--'], cs=[ACC_COLOR],
+                    linestyles=[linestyle_dict[inferencer_name]], cs=[ACC_COLOR],
                     xtickRotate=xtickRotate,
                     markers=['.'],
                     markevery=4,
@@ -136,7 +149,7 @@ def plot_pointonly_notransfer():
         else:
             ax.xaxis.set_label_coords(1.1, -0.2)
 
-    axes[0].legend(bbox_to_anchor=(6.2, 1), ncol=1, frameon=False)
+    axes[2].legend(bbox_to_anchor=(3.9, 1.15), ncol=1, frameon=False)
     #axes[0].legend(bbox_to_anchor=(4.3, 1.5), ncol=3, frameon=False)
     fig.set_size_inches((8.5,2))
     save_fig(fig, outputfile)
@@ -146,12 +159,11 @@ def plot_entities():
     outputfile = FIG_DIR + '/entities.pdf'
     inferencer_names = ['scrabble', 'arka']
     xticks, xticks_labels, yticks, yticks_labels, xlim, ylim, interp_x, \
-        xlabel, ylabel, base_linestyles, xtickRotate = get_grid_params(50,100,10)
-    ylim = (50,100)
+        xlabel, ylabel, base_linestyles, xtickRotate = get_grid_params(40,100,20)
 
     target_sources = [
-        ('ebu3b', None),
-        ('sdh', None),
+        ('ebu3b', [None, 'ap_m']),
+        ('sdh', [None, 'ebu3b']),
         #('sdh', None),
         #('ebu3b', 'ap_m')
     ]
@@ -162,36 +174,36 @@ def plot_entities():
         'arka': ':'
     }
 
-    for ax_num, (ax, (target_building, source_building)) \
+    for ax_num, (ax, (target_building, source_buildings)) \
             in enumerate(zip(axes, target_sources)):
         for inferencer_name in inferencer_names:
             if inferencer_name == 'arka':
-                continue #TODO: disable this once making the result work
+                #continue #TODO: disable this once making the result work
                 if target_building != 'sdh':
                     continue
+                exp_num = 1
+            else:
+                exp_num = EXP_NUM
+            # Notransfer
             xs = []
             ys = []
             xss = []
-            f1s = []
+            accs = []
             mf1s = []
-            for i in range(0, EXP_NUM):
+            for i in range(0, exp_num):
                 with open('result/allentities_notransfer_{0}_{1}_{2}.json'
                           .format(inferencer_name, target_building, i)) as  fp:
                     data = json.load(fp)
                 xss.append([datum['learning_srcids'] for datum in data])
-                if inferencer_name == 'al_hong':
-                    f1s.append([datum['metrics']['f1_micro'] for datum in data])
-                    mf1s.append([datum['metrics']['f1_macro'] for datum in data])
-                else:
-                    f1s.append([datum['metrics']['f1-all'] for datum in data])
-                    mf1s.append([datum['metrics']['macrof1-all'] for datum in data])
+                accs.append([datum['metrics']['accuracy'] for datum in data])
+                mf1s.append([datum['metrics']['macrof1-all'] for datum in data])
             interp_x = list(range(10,
                                   min(250, max([max(xs) for xs in xss]) + 5),
                                   5))
-            f1 = average_data(xss, f1s, interp_x)
+            acc = average_data(xss, accs, interp_x)
             mf1 = average_data(xss, mf1s, interp_x)
             x = interp_x
-            ys = [f1, mf1]
+            ys = [acc, mf1]
             legends = None
             title = building_anon_map[target_building]
             _, plots = plotter.plot_multiple_2dline(
@@ -207,6 +219,53 @@ def plot_entities():
                 markevery=4,
                 markersize=4,
             )
+
+            """
+            # transfer
+            if len(source_buildings) == 1 or inferencer_name == 'arka':
+                continue
+
+            source_building = source_buildings[1]
+
+            xs = []
+            ys = []
+            xss = []
+            accs = []
+            mf1s = []
+            for i in range(0, 1):
+            #for i in range(0, EXP_NUM): #TODO:
+                with open('result/allentities_transfer_{0}_{1}_{2}_{3}.json'
+                          .format(inferencer_name,
+                                  target_building,
+                                  source_building,
+                                  i)) as  fp:
+                    data = json.load(fp)
+                xss.append([datum['learning_srcids'] for datum in data])
+                accs.append([datum['metrics']['accuracy'] for datum in data])
+                mf1s.append([datum['metrics']['macrof1-all'] for datum in data])
+            interp_x = list(range(10,
+                                  min(250, max([max(xs) for xs in xss]) + 5),
+                                  5))
+            acc = average_data(xss, accs, interp_x)
+            mf1 = average_data(xss, mf1s, interp_x)
+            x = interp_x
+            ys = [acc, mf1]
+            legends = None
+            title = building_anon_map[target_building]
+            _, plots = plotter.plot_multiple_2dline(
+                x, ys, xlabel, ylabel, xticks, xticks_labels,
+                yticks, yticks_labels, title, ax, fig,
+                ylim=ylim, xlim=xlim,
+                dataLabels=legends,
+                linestyles = [linestyles[inferencer_name]] * len(ys),
+                #cs = colors,
+                cs = [ACC_COLOR, colors[1]],
+                xtickRotate=xtickRotate,
+                markers=['x', '|'],
+                markevery=4,
+                markersize=4,
+            )
+            """
 
     fig.set_size_inches((4,2))
     for ax in axes:
@@ -255,16 +314,20 @@ def plot_pointonly_transfer():
                                     building_anon_map[target_building])
         linestyles = deepcopy(LINESTYLES)
         for inferencer_name in inferencer_names:
-            if inferencer_name == 'scrabble' and \
-                    not(target_building == 'ebu3b' and
-                        source_building == 'sdh'):
-                continue
             xs = []
             ys = []
             xss = []
             f1s = []
             mf1s = []
-            for i in range(0, EXP_NUM):
+            if inferencer_name == 'scrabble' and target_building == 'ebu3b' and source_building == 'ap_m':
+                #TODO Update this once finished
+                exp_num = 1
+            elif inferencer_name == 'scrabble' and target_building == 'sdh' and source_building == 'ebu3b':
+                exp_num = 2
+            else:
+                exp_num = EXP_NUM
+
+            for i in range(0, exp_num):
                 with open('result/pointonly_transfer_{0}_{1}_{2}_{3}.json'
                           .format(inferencer_name, target_building,
                                   source_building, i)) as  fp:
@@ -277,10 +340,10 @@ def plot_pointonly_transfer():
                     f1s.append([datum['metrics']['f1'] for datum in data])
                     mf1s.append([datum['metrics']['macrof1'] for datum in data])
             xs = xss[0] # Assuming all xss are same.
-            if inferencer_name == 'scrabble':
-                xs = [x - 200 for x in xs]
-                xss[0] = xs
-                xss[1] = xs
+            #if inferencer_name == 'scrabble':
+            #    xs = [x - 200 for x in xs]
+            #    xss[0] = xs
+            #    xss[1] = xs
             interp_x = list(range(10,
                                   min(250, max([max(xs) for xs in xss]) + 5),
                                   5))
@@ -321,20 +384,56 @@ def plot_pointonly_transfer():
     fig.set_size_inches((6,2))
     save_fig(fig, outputfile)
 
-def get_grid_params(ymin=0, ymax=100, ydelta=20):
-    xticks = [0, 10] + list(range(50, 251, 50))
+def get_grid_params(ymin=0, ymax=100, ydelta=20,
+                    xmin=10, xmin2=50, xmax=250, xdelta=50):
+    xticks = [0, xmin] + list(range(xmin2, xmax + 1, xdelta))
     xticks_labels = [''] + [str(n) for n in xticks[1:]]
     yticks = range(ymin, ymax+1, ydelta)
     yticks_labels = [str(n) for n in yticks]
     xlim = (0, xticks[-1])
     ylim = (yticks[0], yticks[-1])
-    interp_x = list(range(10, 250, 5))
+    interp_x = list(range(xmin, xmax, 5))
     xlabel = '# of Samples'
     ylabel = 'Metric (%)'
     linestyles = deepcopy(LINESTYLES)
     xtickRotate = 45
     return xticks, xticks_labels, yticks, yticks_labels, xlim, ylim, interp_x, \
         xlabel, ylabel, linestyles, xtickRotate
+
+def plot_scrabble_zodiac():
+    EXP_NUM = 2
+    building = 'ebu3b'
+    outputfile = FIG_DIR + '/scrabble_zodiac.pdf'
+    fig, ax = plt.subplots(1, 1)
+    xticks, xticks_labels, yticks, yticks_labels, xlim, ylim, interp_x, \
+        xlabel, ylabel, linestyles, xtickRotate = get_grid_params(
+            ymin = 0, ymax = 40, ydelta = 5,
+            xmin = 10, xmin2=50, xmax = 150, xdelta=50)
+    ylabel = 'Count'
+    # Baseline (Naive Zodiac)
+    with open('result/scrabble_zodiac.json', 'r') as fp:
+        res = json.load(fp)
+    x = res['x']
+    y = res['y']
+    ys = [y]
+
+    legends = ['# of fixed samples']
+    title = building_anon_map['ebu3b']
+
+    _, plots = plotter.plot_multiple_2dline(
+        x, ys, xlabel, ylabel, xticks, xticks_labels,
+        yticks, yticks_labels, title, ax, fig, ylim, xlim, legends,
+        linestyles=[linestyles.pop()]*len(ys), cs=colors,
+        xtickRotate=xtickRotate)
+
+    ax.grid(True)
+    ax.tick_params(axis='x', pad=-1.5)
+    #ax.xaxis.set_label_coords(1.1, -0.2)
+
+    ax.legend(bbox_to_anchor=(1.1, 1.35), ncol=1, frameon=False, fontsize='small')
+    #fig.set_size_inches((8,2))
+    fig.set_size_inches((1.5,1.7))
+    save_fig(fig, outputfile)
 
 def plot_quiver_zodiac():
     EXP_NUM = 2
@@ -420,5 +519,6 @@ def plot_quiver_zodiac():
 if __name__ == '__main__':
     #plot_pointonly_notransfer()
     #plot_pointonly_transfer()
-    plot_quiver_zodiac()
+    #plot_quiver_zodiac()
     plot_entities()
+    #plot_scrabble_zodiac()
