24e8975683d58dd4972630cfa9aecf3119360014
Jason Koh <bk7749@gmail.com>
2019-04-07 16:56:47 -0700

    add installer

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from pkg_resources import parse_requirements
+from setuptools import setup, find_packages
+
+__author__ = 'Jason Koh'
+__version__ = '0.0.1'
+
+install_reqs = parse_requirements(open('requirements.txt'))
+
+reqs = [ir.name for ir in install_reqs]
+
+setup(
+    name='plastering',
+    author=__author__,
+    version = __version__,
+    packages = find_packages(),
+    description = '[Plastering](https://github.com/plastering/plastering)',
+    install_requires = reqs,
+)
+
