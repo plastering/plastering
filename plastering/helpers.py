73b02545fbf97cc939922754fdefe11be3ea9631
Jason Koh <bk7749@gmail.com>
2019-08-23 15:44:15 -0700

    code clean-up & bug fixes

---
+++ plastering/helpers.py
@@ -0,0 +1,50 @@
+import argparse
+
+
+## Argparser
+def str2slist(s):
+    s.replace(' ', '')
+    return s.split(',')
+
+def str2ilist(s):
+    s.replace(' ', '')
+    return [int(c) for c in s.split(',')]
+
+def str2bool(v):
+    if v in ['true', 'True']:
+        return True
+    elif v in ['false', 'False']:
+        return False
+    else:
+        assert(False)
+
+argparser = argparse.ArgumentParser()
+argparser.register('type','bool',str2bool)
+argparser.register('type','slist', str2slist)
+argparser.register('type','ilist', str2ilist)
+
+# Adopted from: https://stackoverflow.com/a/21894086
+class bidict(dict):
+    def __init__(self, *args, **kwargs):
+        super(bidict, self).__init__(*args, **kwargs)
+        self.inverse = {}
+        for key, value in self.items():
+            self.inverse.setdefault(value,[]).append(key)
+
+    def __setitem__(self, key, value):
+        if key in self:
+            self.inverse[self[key]].remove(key)
+        super(bidict, self).__setitem__(key, value)
+        self.inverse.setdefault(value,[]).append(key)
+
+    def __delitem__(self, key):
+        self.inverse.setdefault(self[key],[]).remove(key)
+        if self[key] in self.inverse and not self.inverse[self[key]]:
+            del self.inverse[self[key]]
+        super(bidict, self).__delitem__(key)
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
